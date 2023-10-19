c1cc8c75b86baf6a83b879ea6b43876ebc7ac0c3
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-10-19 10:45:14 +0900

    first commit.

---
+++ samples-Alamofire/samples_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  samples_AlamofireApp.swift
+//  samples-Alamofire
+//
+//  Created by Kensuke Nakagawa on 2023/10/19.
+//
+
+import SwiftUI
+
+@main
+struct samples_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
